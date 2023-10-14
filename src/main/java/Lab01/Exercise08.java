f1e961b5555ce92368fcb8bac963622b62a22ddb
katori1307 <voanhquan13072003@gmail.com>
2023-10-14 14:58:01 +0700

    Update...

--- src/main/java/Lab01/Exercise08.java
+++ src/main/java/Lab01/Exercise08.java
@@ -3,19 +3,19 @@ import java.util.Scanner;
 public class Exercise08 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter an integer (n >= 1): ");
+        System.out.print("Enter an integer (n > 5): ");
         int n = scanner.nextInt();
-        if(n < 1) {
+        if(n <= 5) {
             System.out.println("Invalid input");
             return;
         }
         float res = sum_fraction_6(n);
-        System.out.println("1/2 + 2/3 + ... + n/n+1 = " + res);
+        System.out.println("1/2 + 3/4 + ... + 2n+1/2n+2 = " + res);
     }
     public static float sum_fraction_6(int n) {
         if(n == 0)
             return 1.0f/2;
         else
-            return n*1.0f/(n + 1) + sum_fraction_6(n - 1);
+            return (2*n + 1) * 1.0f/(2*n + 2) + sum_fraction_6(n - 1);
     }
 }
